4818d9ac061a0a38614e350108e36ac8911f7127
55day <147132026+55day@users.noreply.github.com>
2023-11-03 00:38:08 +0300

    Add files via upload

---
+++ Массив.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+const int SizeMass1 = 20;
+const int SizeMass2 = 10;
+int num1[SizeMass1];
+int num2[SizeMass2];
+
+void random();
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(0));
+
+    random();
+
+    return 0;
+}
+void random() {
+    for (int i = 0; i < SizeMass1; i++)
+    {
+        num1[i] = 1 + rand() % 20;
+        cout << num1[i]<<" ";
+    }
+    cout << endl;
+    for (int j = 0; j < SizeMass2; j++) {
+         num2[j] = 1 + rand() % 20;
+         cout << num2[j]<<" ";
+    }
+}
---
+++ Массив2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int t = 0;
+int n;
+
+int subtraction();
+void fin();
+
+int main()
+{
+    setlocale(0, "");
+
+    cout << "Введите число" << endl;
+    cin >> n;
+
+    subtraction();
+
+
+    do {
+        fin();
+
+    } while (t % 10 == 0);
+
+    cout << t << endl;
+
+    return 0;
+}
+
+int subtraction()
+{
+    t = 0;
+    while (n > 0) {
+        t += n % 10;
+        n /= 10;
+    }
+
+
+    return n;
+}
+
+void fin()
+{
+    do {
+        cout << t << endl;
+        n = t;
+        subtraction();
+
+    } while (n % 10);
+}
